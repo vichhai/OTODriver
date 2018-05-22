8be992ed6b143d88b53156898d7436cae0bc7dc7
vichhai <kanvichhai@gmail.com>
2018-05-22 17:11:26 +0700

    Initial Commit

---
+++ OTODriver/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OTODriver
+//
+//  Created by iMac007 on 5/22/18.
+//  Copyright © 2018 vichhai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
